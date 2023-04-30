ee8e538e927013fd05ed9f2d6a05b4577d255c96
Matheus Henrique <matheushss2013@gmail.com>
2023-04-30 16:30:30 -0300

    test: create repositories files for test

---
+++ server/src/test/repositories/categories.ts
@@ -0,0 +1,41 @@
+import prisma from "../singleton";
+
+export interface CategoryUpdateData {
+    name?: string;
+}
+
+async function create(name: string) {
+    const category = await prisma.category.create({
+        data: {
+            name,
+        },
+    });
+    return category;
+}
+
+async function list() {
+    const categories = await prisma.category.findMany();
+    return categories;
+}
+
+async function deleteById(id: string) {
+    const category = await prisma.category.delete({
+        where: { id },
+    });
+    return category;
+}
+
+async function updateById(id: string, data: CategoryUpdateData) {
+    const category = await prisma.category.update({
+        where: { id },
+        data,
+    });
+    return category;
+}
+
+export default {
+    create,
+    list,
+    deleteById,
+    updateById,
+};
---
+++ server/src/test/repositories/posts.ts
@@ -0,0 +1,45 @@
+import prisma from "../singleton";
+
+export interface PostUpdateData {
+    authorName?: string;
+    title?: string;
+    content?: string;
+}
+
+async function create(authorName: string, title: string, content: string) {
+    const post = await prisma.post.create({
+        data: {
+            authorName,
+            title,
+            content,
+        },
+    });
+    return post;
+}
+
+async function list() {
+    const posts = await prisma.post.findMany();
+    return posts;
+}
+
+async function deleteById(id: string) {
+    const post = await prisma.post.delete({
+        where: { id },
+    });
+    return post;
+}
+
+async function updateById(id: string, data: PostUpdateData) {
+    const post = await prisma.post.update({
+        where: { id },
+        data,
+    });
+    return post;
+}
+
+export default {
+    create,
+    list,
+    deleteById,
+    updateById,
+};
