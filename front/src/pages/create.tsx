b59c8fe1ce9245dc924dbdd8e2c3b5c57d2df2b1
lucasfranciscosp <lucasfranciscosp7@gmail.com>
2023-04-30 23:46:04 -0300

    feat: send post text to api on save

--- front/src/pages/create.tsx
+++ front/src/pages/create.tsx
@@ -1,18 +1,37 @@
 import dynamic from "next/dynamic";
+import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import "react-markdown-editor-lite/lib/index.css";
 
 const MdEditor = dynamic(() => import("react-markdown-editor-lite"), {
-  ssr: false
+  ssr: false,
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export, react/display-name
 export default function () {
+  const [text, setText] = useState("");
+
+  const submitForm = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    console.log("text", text)
+    const res = await fetch("http://localhost:3000/posts", {
+      method: "POST",
+      body: JSON.stringify({ text }),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
-    <MdEditor
-      style={{ height: "500px" }}
-      // eslint-disable-next-line react/no-children-prop
-      renderHTML={(text) => <ReactMarkdown children={text} />}
-    />
+    <div>
+      <MdEditor
+        style={{ height: "500px" }}
+        value={text}
+        onChange={({ text }) => setText(text)}
+        // eslint-disable-next-line react/no-children-prop
+        renderHTML={(text) => <ReactMarkdown children={text} />}
+      />
+      <button onClick={submitForm}>Submit</button>
+    </div>
   );
 }
