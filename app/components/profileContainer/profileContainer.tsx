71a141a7263ed973d4e5aa33a895538c7110067b
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-06-29 05:37:22 -0300

    feat: add profile page with buttons to change name and logout

--- app/components/profileContainer/profileContainer.tsx
+++ app/components/profileContainer/profileContainer.tsx
@@ -5,6 +5,8 @@ import Image, { StaticImageData } from "next/image";
 import styles from "./profileContainer.module.css";
 import { useSession } from "next-auth/react";
 import { User } from "@prisma/client";
+import { UserRole } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
 const ProfilePictureContainer: React.FC<{ imagePath: StaticImageData }> = ({
   imagePath,
@@ -28,14 +30,37 @@ const DataContainer: React.FC<{ dataName: string; dataInfo: string }> = ({
   return (
     <h3 className={styles.itemContainer}>
       {" "}
-      <span className="font-bold">{dataName}:</span> {dataInfo}
+      <span
+        className="font-bold"
+        style={{ marginLeft: "10px", marginRight: "10px" }}
+      >
+        {dataName}:
+      </span>
+      <div
+        style={{
+          border: "2px solid black",
+          height: "35px",
+          width: "auto",
+          textAlign: "left",
+          paddingLeft: "10px",
+          paddingRight: "10px",
+          overflow: "hidden",
+          borderRadius: "10px",
+          marginRight: "auto",
+        }}
+      >
+        {" "}
+        {dataInfo}{" "}
+      </div>
     </h3>
   );
 };
 
 const ProfileContainer: React.FC<{}> = () => {
-  const { data: session } = useSession();
+  const { data: session, update } = useSession();
   const [user, setUser] = useState<User>();
+  const [userName, setUserName] = useState<string>("");
+  const [rankColor, setRankColor] = useState<string>("");
 
   if (!user) {
     fetch("api/users", {
@@ -48,26 +73,92 @@ const ProfileContainer: React.FC<{}> = () => {
       .then((users: User[]) =>
         users.find((user) => user.id === session?.user.id)
       )
-      .then((user) => setUser(user))
-      .catch((error) => console.error(error))
+      .then((user) => {
+        setUser(user);
+        if (user) setUserName(user.name ? user.name : "");
+      })
+      .catch((error) => console.error(error));
 
     return (
       <div className={styles.profileContainer}>
-        <p>Loading...</p>
+        <p>Carregando...</p>
       </div>
     );
   }
-  console.log("User logged in profile page")
-  console.log(user);
   return (
     <div className={styles.profileContainer}>
       <ProfilePictureContainer imagePath={profileIcon} />
       <div className={styles.doubleItemContainer}>
-        <DataContainer dataName="Nome" dataInfo={user?.name ? user.name : ""} />
+        <h3 className={styles.itemContainer}>
+          {" "}
+          <span
+            className="font-bold"
+            style={{ marginLeft: "10px", marginRight: "10px" }}
+          >
+            Nome:
+          </span>
+          <textarea
+            defaultValue={user.name ? user.name : "Ainda sem nome😔"}
+            onChange={(e) => setUserName(e.target.value)}
+            className={styles.textArea}
+          ></textarea>
+        </h3>
         <DataContainer
           dataName="Conta"
-          dataInfo={user?.role ? "Estudante📚" : "Professor🏛️"}
+          dataInfo={
+            user.role === UserRole.STUDENT ? "Estudante📚" : "Professor🏛️"
+          }
         />
+      </div>
+      <div className={styles.doubleItemContainer}>
+        <DataContainer dataName="Problemas Resolvidos" dataInfo={"78"} />
+        <DataContainer dataName="Problemas Tentados" dataInfo={"90"} />
+      </div>
+      <div>Ranking vai aqui</div>
+      <div
+        className={styles.itemContainer}
+        style={{
+          width: "100%",
+          marginTop: "auto",
+          marginBottom: "10px",
+          height: "fit-content",
+        }}
+      >
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-5 px-10"
+          style={{
+            borderRadius: "15px",
+            display: "inline-flex",
+            alignItems: "center",
+            marginRight: "auto",
+            marginLeft: "auto",
+          }}
+          onClick={async () => {
+            const newUser = await fetch(`api/users?id=${user.id}`, {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ name: userName }),
+            });
+            console.log(newUser);
+          }}
+        >
+          Alterar nome
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-5 px-10"
+          style={{
+            borderRadius: "15px",
+            display: "inline-flex",
+            alignItems: "center",
+            marginRight: "auto",
+            marginLeft: "auto",
+          }}
+          onClick={() => signOut()}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
