71687498435b99878a033f61130a53a6b08e5aa7
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-06-29 11:26:36 -0300

    feat: update middleware to send users to start page when not logged from profile page

--- middleware.ts
+++ middleware.ts
@@ -3,47 +3,54 @@ import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-    async function middleware(req) {
-        const token = await getToken({ req });
-        const isAuth = !!token;
-        const isLoginPage =
-            req.nextUrl.pathname.startsWith("/login") ||
-            req.nextUrl.pathname.startsWith("/register");
+  async function middleware(req) {
+    const token = await getToken({ req });
+    const isAuth = !!token;
+    const isLoginPage =
+      req.nextUrl.pathname.startsWith("/login") ||
+      req.nextUrl.pathname.startsWith("/register");
+    const isProfilePage = req.nextUrl.pathname.startsWith("/profile");
 
-        if (isLoginPage) {
-            if (isAuth) {
-                return NextResponse.redirect(new URL("/", req.url));
-            }
+    if (isProfilePage) {
+      if (!isAuth) return NextResponse.redirect(new URL("/", req.url));
 
-            return null;
-        }
-
-        if (!isAuth) {
-            let from = req.nextUrl.pathname;
-            if (req.nextUrl.search) {
-                from += req.nextUrl.search;
-            }
-
-            return NextResponse.redirect(
-                new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
-            );
-        }
-
-        return null; // Allow access to all other pages for authenticated users
-    },
-    {
-        callbacks: {
-            async authorized() {
-                // This is a work-around for handling redirect on auth pages.
-                // We return true here so that the middleware function above
-                // is always called.
-                return true;
-            },
-        },
+      return null;
     }
+
+    if (isLoginPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+
+      return null;
+    }
+
+    if (!isAuth) {
+      let from = req.nextUrl.pathname;
+      if (req.nextUrl.search) {
+        from += req.nextUrl.search;
+      }
+
+      return NextResponse.redirect(
+        new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
+      );
+    }
+
+    return null; // Allow access to all other pages for authenticated users
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true;
+      },
+    },
+  }
 );
 
 //default endpoint?
 export const config = {
-    matcher: ["/login", "/register", "/profile"],
+  matcher: ["/login", "/register", "/profile"],
 };
