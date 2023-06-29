45adcd7948e1bc3c1ef2945284f7a5fc205be0d6
Vinícius Alves Damasceno <vinidamasceno2010@gmail.com>
2023-06-29 11:42:21 -0300

    feat: create searchbar for classes page

---
+++ app/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./SearchBar.module.css"
+
+const SearchBar: React.FC = () => {
+    return (
+        <div className={style.input_wrapper}>
+            <input type="text" />
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default SearchBar
